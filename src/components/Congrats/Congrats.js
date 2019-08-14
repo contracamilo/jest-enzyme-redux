7d3e5f6bfb34bc88364ca7422c3128d230ee9321
Camilo Rivera Quintero <camilo.rivera@globant.com>
2019-08-14 14:22:18 -0500

    Congrats Comp

---
+++ src/components/Congrats/Congrats.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+/**
+ * Functional react component for congratulatory message
+ * @function
+ * @returns {JSX.Element} - Rendered component (or null if success' prop) is set
+ */
+
+const Congrats = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Congrats;
